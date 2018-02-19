3db6f21fae980c5698097db320d42c37659bb1fb
Shreyas Hukkeri <hshreyas12@gmail.com>
2018-02-19 13:14:10 -0800

    Progress Update

--- train-tflearn.py
+++ train-tflearn.py
@@ -7,8 +7,8 @@ from tflearn.metrics import R2
 from tensorflow.contrib.losses import mean_pairwise_squared_error
 import tensorflow as tf
 
-model_version = "4"
-model_run = "2"
+model_version = "5"
+model_run = "1"
 model_file = '../Models/'+model_version+'/'+model_run+'/model'
 image_dimensions = (240, 320)
 
@@ -34,29 +34,10 @@ with tf.device('/gpu:0'):
     conv = input_data(shape=[None, image_dimensions[0], image_dimensions[1], 1], dtype=tf.float32)
     conv = conv-tf.reduce_min(conv)
     conv = (conv/tf.reduce_max(conv))
-    conv = conv_2d(conv, 16, 9, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 9, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 9, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 9, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 9, activation='leaky_relu')
-    #conv = max_pool_2d(conv, 2, strides=2)
+    conv = conv_2d(conv, 32, 9, activation='leaky_relu')
     conv = conv_2d(conv, 16, 7, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 7, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 7, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 7, activation='leaky_relu')
-    #conv = max_pool_2d(conv, 2, strides=2)
-    conv = conv_2d(conv, 16, 5, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 5, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 5, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 5, activation='leaky_relu')
-    #conv = max_pool_2d(conv, 2, strides=2)
-    conv = conv_2d(conv, 16, 3, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 3, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 3, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 3, activation='leaky_relu')
-    conv = dropout(conv, keep_prob=0.75)
-    conv = fully_connected(conv, 1024)
-    conv = dropout(conv, keep_prob=0.75)
+    conv = conv_2d(conv, 8, 5, activation='leaky_relu')
+    conv = conv_2d(conv, 4, 3, activation='leaky_relu')
     conv = fully_connected(conv, 2)
     conv = regression(conv, optimizer='adam', metric=R2(),
                          loss=mean_pairwise_squared_error,
