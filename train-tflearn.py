2ad66ae26815e5a963f3d0608e8aee07a29c49bb
Shreyas Hukkeri <hshreyas12@gmail.com>
2018-02-19 14:52:12 -0800

    Progress Update

--- train-tflearn.py
+++ train-tflearn.py
@@ -7,7 +7,7 @@ from tflearn.metrics import R2
 from tensorflow.contrib.losses import mean_pairwise_squared_error
 import tensorflow as tf
 
-model_version = "7"
+model_version = "8"
 model_run = "1"
 model_file = '../Models/'+model_version+'/'+model_run+'/model'
 image_dimensions = (240, 320)
@@ -19,6 +19,10 @@ Y = tr_f['Y']
 val_f = h5py.File("../Data/val-"+str(image_dimensions[0])+"x"+str(image_dimensions[1])+".hdf5", 'r')
 X_val = val_f['X']
 Y_val = val_f['Y']
+
+te_f = h5py.File("../Data/test-"+str(image_dimensions[0])+"x"+str(image_dimensions[1])+".hdf5", 'r')
+X_test = te_f['X']
+Y_test = te_f['Y']
 
 chunk_size = 10000
 batch_size = 1000
@@ -34,7 +38,8 @@ with tf.device('/gpu:0'):
     conv = input_data(shape=[None, image_dimensions[0], image_dimensions[1], 1], dtype=tf.float32)
     conv = conv-tf.reduce_min(conv)
     conv = -1*((conv/tf.reduce_max(conv))-0.5)
-    conv = 100*flatten(conv)
+    conv = flatten(conv)
+    conv = fully_connected(conv, 1024, activation='sigmoid')
     conv = fully_connected(conv, 2)
     conv = regression(conv, optimizer='adam', metric=R2(),
                          loss=mean_pairwise_squared_error,
@@ -46,23 +51,34 @@ with tf.device('/gpu:0'):
                 validation_batch_size=64, show_metric=True, run_id=str("Model-"+model_version+'-'+model_run),
               snapshot_step=50)
 '''
+val_set_length = len(Y_val)
+total_chunks_done = 0
 for j in range(num_epochs):
-    total_batches_done = 0
     for i in range(num_batches):
         start = chunk_size*i
         stop = start+chunk_size
+        val_start = np.random.randint(0, val_set_length-val_batch_size-1)
+        val_stop = val_start+val_batch_size
         with tf.device('/GPU:0'):
             model.fit(X[start:stop], Y[start:stop], n_epoch=1, shuffle=True, batch_size=batch_size,
-                      validation_set=(X_val[0:val_batch_size], Y_val[0:val_batch_size]),
+                      validation_set=(X_val[val_start:val_stop], Y_val[val_start:val_stop]),
                       validation_batch_size=val_batch_size, show_metric=True,
                       run_id=str("Model-"+model_version+'-'+model_run), snapshot_step=50, snapshot_epoch=False)
-        pred = model.predict([X[i]])
+        rand_example = np.random.randint(0, val_set_length)
+        pred = model.predict([X_val[rand_example]])
         print("Model Prediction:" + str(pred[0]))
-        print("Actual Value:" + str(Y[i]))
-        print("Pixel Distance:" + str(np.sqrt(np.sum(np.square(pred[0]-Y[i])))))
-        total_batches_done += 1
-        if total_batches_done%50 ==0:
+        print("Actual Value:" + str(Y_val[rand_example]))
+        print("Pixel Distance:" + str(np.sqrt(np.sum(np.square(pred[0]-Y_val[rand_example])))))
+        total_chunks_done += 1
+        print("Num Chunks Completed: "+str(total_chunks_done))
+        if total_chunks_done%5 ==0:
             model.save(model_file=str(model_file+str(j)+str(i)))
+            num_under_five = 0
+            for t in range(len(Y_test)):
+                pred = model.predict([X_test[t]])
+                dist = np.sqrt(np.sum(np.square(pred[0] - Y_test[t])))
+                if (dist < 5):
+                    num_under_five += 1
 
 tr_f.close()
 val_f.close()
