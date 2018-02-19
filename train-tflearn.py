c6242ee5273e8bd453ee149f9fe6945462afda74
Shreyas Hukkeri <hshreyas12@gmail.com>
2018-02-19 13:56:30 -0800

    Progress Update

--- train-tflearn.py
+++ train-tflearn.py
@@ -7,7 +7,7 @@ from tflearn.metrics import R2
 from tensorflow.contrib.losses import mean_pairwise_squared_error
 import tensorflow as tf
 
-model_version = "5"
+model_version = "6"
 model_run = "1"
 model_file = '../Models/'+model_version+'/'+model_run+'/model'
 image_dimensions = (240, 320)
@@ -20,9 +20,9 @@ val_f = h5py.File("../Data/val-"+str(image_dimensions[0])+"x"+str(image_dimensio
 X_val = val_f['X']
 Y_val = val_f['Y']
 
-chunk_size = 2048
-batch_size = 64
-val_batch_size = 64
+chunk_size = 10000
+batch_size = 750
+val_batch_size = 750
 num_batches = int(len(Y)/chunk_size)
 num_epochs = 5
 
@@ -33,15 +33,11 @@ tflearn.init_graph(num_cores=8, gpu_memory_fraction=0.9, soft_placement=True)
 with tf.device('/gpu:0'):
     conv = input_data(shape=[None, image_dimensions[0], image_dimensions[1], 1], dtype=tf.float32)
     conv = conv-tf.reduce_min(conv)
-    conv = (conv/tf.reduce_max(conv))
-    conv = conv_2d(conv, 32, 9, activation='leaky_relu')
-    conv = conv_2d(conv, 16, 7, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 5, activation='leaky_relu')
-    conv = conv_2d(conv, 4, 3, activation='leaky_relu')
+    conv = (conv/tf.reduce_max(conv))-0.5
     conv = fully_connected(conv, 2)
     conv = regression(conv, optimizer='adam', metric=R2(),
                          loss=mean_pairwise_squared_error,
-                         learning_rate=0.0001)
+                         learning_rate=0.001)
     model = tflearn.DNN(conv,tensorboard_dir=str("../Models/" + model_version + '/' + model_run + '/Tensorboard/'),
                         tensorboard_verbose=1)
     '''model.fit(X, Y, n_epoch=1, shuffle=True, batch_size=64,
