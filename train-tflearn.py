41e1ec94e76a34b6f8aaba85139f3091f62369da
Shreyas Hukkeri <hshreyas12@gmail.com>
2018-02-24 18:14:27 -0800

    Progress Update

--- train-tflearn.py
+++ train-tflearn.py
@@ -6,7 +6,7 @@ from tflearn.layers.estimator import regression
 from tflearn.metrics import R2
 import tensorflow as tf
 
-model_version = "20"
+model_version = "21"
 model_run = "1"
 model_file = '../Models/'+model_version+'/'+model_run+'/model'
 image_dimensions = (240, 320)
@@ -24,9 +24,9 @@ X_test = te_f['X']
 Y_test = te_f['Y']
 
 chunk_size = 10240
-batch_size = 256
-val_batch_size = 256
-test_batch_size = 256
+batch_size = 512
+val_batch_size = 512
+test_batch_size = 512
 num_batches = int(len(Y)/chunk_size)
 num_epochs = 20
 
@@ -39,29 +39,20 @@ with tf.device('/gpu:0'):
     conv = conv-tf.reduce_min(conv)
     input_conv = -1*((conv/tf.reduce_max(conv))-0.5)
 
-    conv = conv_2d(input_conv, 8, 15, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 15, activation='leaky_relu')
+    conv = conv_2d(input_conv, 8, 9, activation='leaky_relu')
     conv = max_pool_2d(conv, 2, 2)
     conv = conv_2d(conv, 8, 9, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 9, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 9, activation='leaky_relu')
     conv = max_pool_2d(conv, 2, 2)
     conv = conv_2d(conv, 8, 7, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 7, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 7, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 7, activation='leaky_relu')
     conv = max_pool_2d(conv, 2, 2)
-    conv = conv_2d(conv, 8, 5, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 5, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 5, activation='leaky_relu')
-    conv = conv_2d(conv, 8, 5, activation='leaky_relu')
+    conv = conv_2d(conv, 8, 7, activation='leaky_relu')
     conv = max_pool_2d(conv, 2, 2)
     conv = conv_2d(conv, 8, 3, activation='leaky_relu')
     conv = conv_2d(conv, 8, 3, activation='leaky_relu')
     conv = conv_2d(conv, 8, 3, activation='leaky_relu')
     conv = conv_2d(conv, 8, 3, activation='leaky_relu')
-
     conv = flatten(conv)
+    conv = fully_connected(conv, 128, activation='leaky_relu')
     conv = fully_connected(conv, 2)
     conv = regression(conv, optimizer='adam', metric=R2(),
                          loss=tf.losses.mean_squared_error,
