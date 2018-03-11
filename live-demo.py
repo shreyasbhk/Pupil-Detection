254724edeca8eb5905dbb4cbe8464bfb6b0da112
Shreyas Hukkeri <hshreyas12@gmail.com>
2018-03-10 16:11:08 -0800

    Progress Update

--- live-demo.py
+++ live-demo.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 from tflearn.layers.core import *
 from tflearn.layers.conv import *
 from tflearn.layers.estimator import regression
@@ -36,12 +37,40 @@ model.load(model_file)
 def get_camera_image(cap):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return gray
+    img = cv2.resize(gray, (image_dimensions[1], image_dimensions[0]))
+    return img
+
+
+def get_marked_image(img):
+    original_img = img
+    pred_img = np.expand_dims(img.astype(np.float32), axis=-1)
+    pred = model.predict([pred_img])
+    print((int(pred[0][0]), int(pred[0][1])))
+    color_img = cv2.cvtColor(original_img, cv2.COLOR_GRAY2BGR)
+    cv2.circle(color_img, (int(pred[0][0]/2), int(pred[0][1]/2)), 10, (0,0,255), -1)
+    return color_img
+
+
+def get_prediction(img):
+    pred_img = np.expand_dims(img.astype(np.float32), axis=-1)
+    pred = model.predict([pred_img])
+    return pred
 
 
 if __name__ == "__main__":
-    capt = cv2.VideoCapture(0)
+    capt = cv2.VideoCapture(1)
+    capt.set(cv2.CAP_PROP_BRIGHTNESS, 3)
+    start_time = time.time()
+    num_frames = 0
     while True:
         image = get_camera_image(capt)
-        cv2.imshow('frame', image)
+        image = get_prediction(image)
+        num_frames += 1
+        if(num_frames%30==0):
+            last_time = time.time()
+            print(num_frames/(last_time-start_time))
+            num_frames=0
+            start_time = last_time
+        #image = get_marked_image(image)
+        #cv2.imshow('frame', image)
         cv2.waitKey(1)
